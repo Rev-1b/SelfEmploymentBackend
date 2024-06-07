0ec5f627d403da983540e8b82404c721feb111fe
Alex Luenko <new.luenkoaleksei@gmail.com>
2024-06-07 17:33:00 +0400

    Commit before CustomerViewSet change

--- backend/customers/views.py
+++ backend/customers/views.py
@@ -6,14 +6,17 @@ from customers.models import Customer
 from customers.serializers import CustomerPageSerializer, CustomerDetailSerializer
 
 
-class CustomerPageView(generics.GenericAPIView):
-    queryset = Customer.objects.all()
-    serializer_class = CustomerPageSerializer
-
-    def get(self, request):
-        queryset = self.get_queryset().filter(user=request.user)
-        serializer = CustomerPageSerializer(queryset, many=True)
-        return Response(serializer.data)
+# class CustomerPageView(generics.GenericAPIView):
+#     queryset = Customer.objects.all()
+#     serializer_class = CustomerPageSerializer
+#
+#     def get(self, request):
+#         if self.kwargs.get('pk') is None:
+#             queryset = self.get_queryset().filter(user=request.user)
+#             serializer = CustomerPageSerializer(queryset, many=True)
+#             return Response(serializer.data)
+#         else:
+#             return
 
 
 class CustomerDetailViewSet(mixins.CreateModelMixin,
@@ -25,6 +28,23 @@ class CustomerDetailViewSet(mixins.CreateModelMixin,
     queryset = Customer.objects.all()
     serializer_class = CustomerDetailSerializer
 
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset().filter(user=request.user)
+        serializer = CustomerPageSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
+
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+
+    def partial_update(self, request, *args, **kwargs):
+        return super().partial_update(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        return super().destroy(request, *args, **kwargs)
+
     # def get_serializer(self, *args, fields_type=None, **kwargs):
     #     serializer_class = self.get_serializer_class()
     #     kwargs.setdefault('context', self.get_serializer_context())
@@ -32,18 +52,3 @@ class CustomerDetailViewSet(mixins.CreateModelMixin,
     #     if fields_type is None:
     #         serializer_class.Meta.fields = serializer_class.COMMON_FIELDS
     #     return serializer_class(*args, **kwargs)
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def post(self, request):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-
-    def patch(self, request, *args, **kwargs):
-        pass
-
-    def delete(self, request, *args, **kwargs):
-        pass
