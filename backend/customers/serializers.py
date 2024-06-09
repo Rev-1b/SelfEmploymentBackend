1b4831a67a06d4efb5deaeda06f2b95428ba620c
Alex Luenko <new.luenkoaleksei@gmail.com>
2024-06-09 18:11:47 +0400

    Added customer CRUDL endpoint
    Need to work on serializer fields change logic depends on customer type

--- backend/customers/serializers.py
+++ backend/customers/serializers.py
@@ -32,6 +32,9 @@ class CustomerDetailSerializer(serializers.ModelSerializer):
     IE_FIELDS = ['post_address', 'inn', 'place_of_residence', 'ogrnip']
     EXTERNAL_FIELDS = ['requisites', 'contacts']
 
+    FIELDS_TO_UPDATE = ['additional_id', 'customer_name', 'post_address', 'inn', 'full_company_name', 'orgn', 'kpp',
+                        'legal_address', 'okpo', 'okved', 'place_of_residence', 'ogrnip']
+
     class Meta:
         model = Customer
         read_only_fields = ['id', 'date_created']
@@ -42,8 +45,8 @@ class CustomerDetailSerializer(serializers.ModelSerializer):
         ]
 
     def create(self, validated_data):
-        requisites_data = validated_data.pop('requisites')
-        contacts_data = validated_data.pop('contacts')
+        requisites_data = validated_data.pop('requisites', None)
+        contacts_data = validated_data.pop('contacts', None)
         user = self.context.get('request').user
 
         customer = Customer.objects.create(user=user, **validated_data)
@@ -57,4 +60,25 @@ class CustomerDetailSerializer(serializers.ModelSerializer):
         return customer
 
     def update(self, instance, validated_data):
-        pass
+        requisites_data = validated_data.pop('requisites', None)
+        contacts_data = validated_data.pop('contacts', None)
+
+        for field in self.FIELDS_TO_UPDATE:
+            setattr(instance, field, validated_data.get(field, getattr(instance, field)))
+
+        if requisites_data is not None:
+            if hasattr(instance, 'requisites'):
+                CustomerRequisites.objects.filter(customer=instance).delete()
+            CustomerRequisites.objects.bulk_create(
+                [CustomerRequisites(customer=instance, **data) for data in requisites_data]
+            )
+
+        if contacts_data is not None:
+            if hasattr(instance, 'contacts'):
+                CustomerContacts.objects.filter(customer=instance).delete()
+            CustomerContacts.objects.bulk_create(
+                [CustomerContacts(customer=instance, **data) for data in contacts_data]
+            )
+
+        instance.save()
+        return instance
