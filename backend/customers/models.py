a06a18ccf353e1e04118a7f37abc708a8ab23db4
Alex Luenko <new.luenkoaleksei@gmail.com>
2024-07-05 15:34:42 +0400

    Added CustomerPassport model for common customer. Changed customer serializers. Added new contacts and requisites viewsets for optimisation

--- backend/customers/models.py
+++ backend/customers/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from users.models import CustomUser, CustomModel
+from users.models import CustomUser, CustomModel, Passport
 
 
 class Customer(CustomModel):
@@ -23,7 +23,7 @@ class Customer(CustomModel):
     customer_name = models.CharField(max_length=150, verbose_name='ФИО/Сокращенное название')
     customer_type = models.CharField(max_length=2, choices=CustomerTypes)
 
-    # Common fields for LLC and IE
+    # Same fields for LLC and IE
     post_address = models.CharField(max_length=150, verbose_name='Почтовый адрес', null=True, blank=True)
     inn = models.CharField(verbose_name='Идентификационный номер налогоплательщика', null=True, blank=True)
 
@@ -43,6 +43,24 @@ class Customer(CustomModel):
     ogrnip = models.CharField(
         verbose_name='Основной государственный регистрационный номер индивидуального предпринимателя', null=True,
         blank=True)
+
+
+class CustomerPassport(CustomModel):
+    class Meta(CustomModel.Meta):
+        verbose_name = 'Паспорт заказчика'
+        verbose_name_plural = 'Паспорта заказчиков'
+        # unique_together = ['series', 'number']
+
+    def __str__(self):
+        return f'{self.series}{self.number}'
+
+    customer = models.OneToOneField(to=Customer, on_delete=models.CASCADE, related_name='passport',
+                                    null=True, blank=True, verbose_name='Заказчик')
+    series = models.CharField(max_length=150, verbose_name='Серия паспорта')
+    number = models.CharField(max_length=150, verbose_name='Номер паспорта')
+    release_date = models.DateField(max_length=150, verbose_name='Дата выдачи')
+    issued = models.CharField(max_length=150, verbose_name='Выдан')
+    unit_code = models.CharField(max_length=7, verbose_name='Код подразделения')
 
 
 class CustomerRequisites(CustomModel):
